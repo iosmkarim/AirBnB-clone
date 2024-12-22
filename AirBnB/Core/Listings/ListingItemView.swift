c2cdaf241fa8c3099b47c47026d48bfc97f9c3f9
Md Rezaul Karim <iosmkarim@gmail.com>
2024-12-21 22:54:32 -0500

    [AIRBNB-001] add explore view

---
+++ AirBnB/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingItemView.swift
+//  AirBnB
+//
+//  Created by Md Rezaul Karim on 12/21/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
