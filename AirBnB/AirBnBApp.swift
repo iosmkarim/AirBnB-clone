7cd36c8677e37dbf0e6406c2fd25f23e1b3125c3
Md Rezaul Karim <iosmkarim@gmail.com>
2024-12-21 19:55:55 -0500

    Initial Commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Md Rezaul Karim on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirBnB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AirBnB
+//
+//  Created by Md Rezaul Karim on 12/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
