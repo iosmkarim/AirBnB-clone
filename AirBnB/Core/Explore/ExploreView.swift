fea322bea1fab242210fa5b4bfb50d88c52cf60e
Md Rezaul Karim <iosmkarim@gmail.com>
2024-12-21 22:57:24 -0500

    [AIRBNB-001] added missing source files

--- AirBnB/Core/Explore/ExploreView.swift
+++ AirBnB/Core/Explore/ExploreView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                SearchAndFilterBar()
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { listing in
+                        ListingItemView()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                }
+            }
+        }
     }
 }
 
--- AirBnB/Core/Listings/ListingItemView.swift
+++ AirBnB/Core/Listings/ListingItemView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    
+    var images = [
+    
+    "listing-1",
+    "listing-2",
+    "listing-3",
+    "listing-4"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+           //images
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    Image(image)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+                .frame(height: 270)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .tabViewStyle(.page) 
+                
+          // listing details
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    HStack(spacing: 2) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+
+                }
+                
+                Spacer()
+                //ratings
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+        .padding()
     }
 }
 
